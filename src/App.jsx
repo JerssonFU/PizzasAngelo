a55ffbbc4c867ce9f7f110b7ac4d3d2da256fd8e
JerssonFU <jersson.zynts@gmail.com>
2024-12-21 19:18:52 -0500

    Se agrega enlaces en el navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import NavBar from './components/NavBar'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+
+const Home = () => <div>Inicio</div>;
+const About = () => <div>Acerca de nosotros</div>;
+const Services = () => <div>Servicios</div>;
+const Contact = () => <div>Contacto</div>;
 
 const App = () => {
   return (
-    <div>
-      <NavBar/>
-    </div>
-  )
-}
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import './Navbar.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css'; // Mantenemos los estilos externos
 
 const NavBar = () => {
   return (
     <header className="header">
-        <a href="/" className="logo">PizzasAngelo</a>
-
-        <nav className="navbar">
-            <a href="/">Home</a>
-            <a href="/">About</a>
-            <a href="/">Shop</a>
-            <a href="/">services</a>
-            <a href="/">Contact</a>
-
-
-        </nav>
+      <div className="logo">MiLogo</div>
+      <nav className="navbar">
+        <Link to="/">Inicio</Link>
+        <Link to="/about">Acerca de</Link>
+        <Link to="/services">Servicios</Link>
+        <Link to="/contact">Contacto</Link>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
